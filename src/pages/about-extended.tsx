55d6ed841adda5c74577c821523bcba23df9420d
Tanner Johnson <tlj21@duke.edu>
2018-07-06 09:35:29 -0600

    content deltas

--- src/pages/about-extended.tsx
+++ src/pages/about-extended.tsx
@@ -5,44 +5,39 @@ export default () => {
   return (
     <Container>
       <Segment vertical>
-        <Header as="h2">
-          <Icon name="heart" />
-          <Header.Content>
-            Our Story
-          </Header.Content>
-        </Header>
+        <Container>
+          <Header as="h1" textAlign='center'>
+            <Icon name="heart" />
+            <Header.Content textAlign='center'>
+              Our Story
+            </Header.Content>
+          </Header>
+        </Container>
       </Segment>
       <Segment vertical>
         <p>
-          Durham, North Carolina, is a community home to one of the nation's most robust tech industries, 
-          and yet accessible K-12 CS education programming remains scarce. Our team conducted <a href="https://drive.google.com/file/d/1mzQVjkViLtheaF52JBOaxXVTQF5Y6Xyc/view?usp=sharing">award-winning 
-          research</a> on the state of CS education, finding that what Durham needs is not only freely available, 
-          pre-packaged CS curricula, but also community-tailored, student-centered CS education. This is why we 
-          developed <a href="http://www.dukechronicle.com/article/2017/11/duke-students-develop-after-school-program-to-bring-computer-science-to-low-income-middle-schoolers">Mobile Citizens</a>, 
-          which empowers students to utilize CS to fully participate in their communities 
-          by creating knowledge, sharing ideas, and solving problems relevant to their communities.
+          <font size="3.5">Our story begins in Durham, North Carolina, a community home to one of the most robust tech industries and least accessible 
+          computer science (CS) education programming. We conducted <a href="https://drive.google.com/file/d/1mzQVjkViLtheaF52JBOaxXVTQF5Y6Xyc/view?usp=sharing">award-winning 
+          research</a> on the state of CS education, finding what Durham needs is 
+          community-tailored, student-centered CS education. So, we created <a href="http://www.dukechronicle.com/article/2017/11/duke-students-develop-after-school-program-to-bring-computer-science-to-low-income-middle-schoolers">Mobile Citizens</a>, a program that empowers middle school 
+          students to create mobile apps relevant to their communities’ needs. </font> 
         </p>
         <p>
-          To create the curriculum for Mobile Citizens, we first turned to existing open-access resources like 
-          Code.org’s <a href="https://code.org/educate/csd">CS Discoveries</a> and Google’s <a href="https://csfirst.withgoogle.com/en/home">CS First</a>. After evaluating the widespread high-quality open education 
-          resources currently available, we realized none quite fit the Durham community’s unique needs. With mentorship 
-          from the <a href="https://ospri.ssri.duke.edu/">OSPRI</a> (Open Source Pedagogy, Research + Innovation) initiative and in consultation with Duke CS and 
-          education professors, we adapted Code.org’s CS Discoveries curriculum to reflect the more civic-minded ethos 
-          that our students in Durham have. Our student-generated, community-tailored Mobile Citizens curriculum has 
-          made a more personal, meaningful impact than curricula that is “one-size-fits-all.”
+          <font size="3.5">To create the curriculum for Mobile Citizens, we first turned to Code.org’s <a href="https://code.org/educate/csd">CS Discoveries</a> and Google’s <a href="https://csfirst.withgoogle.com/en/home">CS First</a>, 
+          but we realized none quite fit the Durham community’s unique needs. With mentorship from the <a href="https://ospri.ssri.duke.edu/">OSPRI</a> (Open Source Pedagogy, Research + Innovation) initiative and Duke 
+          professors, we adapted CS Discoveries to make our Mobile Citizens curriculum civically minded and interactive, a curriculum 
+          more meaningful than a curriculum that is “one-size-fits-all.”</font> 
         </p>
         <p>
-          We are thrilled that our Mobile Citizens curriculum is being expanded to additional middle schools and after 
-          school programs in our community. However, the problem of access to relevant CS education extends past Durham; 
-          every community deserves CS education that complements its unique diversity and distinctiveness. CSbyUs employs 
-          the same pedagogical model of Mobile Citizens, but extends the curriculum beyond CS Discoveries so that the 
-          framework that has served our Durham students can also serve students in downtown Houston, small suburbs of Idaho, 
-          and all other communities. It was undergraduates who created more personal, relevant CS education with our first 
-          curriculum, Mobile Citizens, and we believe that CSbyUs can empower more undergraduates to do the same.
+          <font size="3.5">We are thrilled that our Mobile Citizens curriculum has expanded to three additional Durham communities. 
+            However, the problem of access to relevant CS education extends past Durham; every community deserves CS education that 
+            complements its diversity and distinctiveness. CSbyUs is extending the pedagogy that served our Durham students to also serve 
+            students in downtown Houston, suburbs of Idaho suburbs, and all other communities. It was undergraduates who created more personalized 
+            CS education with our first curriculum, Mobile Citizens, and we believe that CSbyUs can empower more undergraduates to do the same.</font> 
         </p>
       </Segment>
     <Segment vertical>
-        <Header as="h2">
+        <Header as="h1">
           <Icon name="globe" />
           <Header.Content>
             Learning Model
@@ -51,19 +46,11 @@ export default () => {
     </Segment>
     <Segment vertical>
       <p>
-        CSbyUs combines a personalized learning model with the power of 21st century information technology to 
-        expand access to high-impact computer science education and close the diversity gap in STEM fields. 
-        CSbyUs supports networks of undergraduate students to customize existing open-access CS curricula to meet 
-        the needs of local learning communities and deploy their new open-source curricula in middle and high schools 
-        and after school programs.
+        <font size="3.5">CSbyUs supports a network of undergraduate students to customize and deploy CS curricula to middle school 
+        students in their area. We further harness the uniqueness of our learners by delivering curricula that is: 
+        1) <b>student-centered</b> 2) <b>project-based</b> and 3) informed by <b>design thinking</b>. CSbyUs enables CS to be taught by students, 
+        for students.</font> 
       </p>
-      <ul>
-        <li>Student-centered</li>
-        <li>Community-embedded</li>
-        <li>Project-based</li>
-        <li>Informed by design thinking</li>
-        <li>By students, for students</li>
-      </ul>
       </Segment>
     </Container>
   );
--- src/pages/what-we-do.tsx
+++ src/pages/what-we-do.tsx
@@ -1,24 +1,87 @@
 import * as React from "react";
-import { Header, Container, Segment, Icon } from "semantic-ui-react";
+import { Header, Container, Segment, Icon, Grid, Icon, Button} from "semantic-ui-react";
+import {ExampleImage} from "../components/image-renders.jsx";
 
 export default () => {
   return (
     <Container>
-      <Segment vertical>
-        <Header as="h2">
-          <Icon name="heart" />
-          <Header.Content>
-            What We Do
-          </Header.Content>
-        </Header>
+        <Segment>
+            <Container style={{padding: "30px"}}>
+                <Header as='h2' size='huge' icon textAlign='center' >
+                <Icon name='users' circular />
+                    <Header.Content>What We Do</Header.Content>
+                </Header>
+                <Header as='h3' textAlign='center' >
+                    <Header.Content>
+                        The mission of CSbyUs is to ignite future change-agents by providing quality computer science education
+                        to students from under-resourced communities. 
+                    </Header.Content>
+                </Header>
+            </Container>
+            <Segment vertical className="stripe alternate feature" style={{padding: "50px"}}>
+                <Grid columns="3" textAlign="center" divided relaxed stackable className="container" style={{color: "gray"}}>
+                    <Grid.Row> 
+                        <Grid.Column>
+                            <Icon name="pencil alternate" size='huge' style={{color: "#e91e63"}}></Icon>
+                            <Header >
+                                <font size="4">The Problem</font>
+                            </Header>
+                              <Button icon fluid basic href="#problem">
+                                <Icon name='angle down' />
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Icon name="lightbulb outline" size='huge' style={{color: "#e91e63"}}></Icon>
+                            <Header >
+                                <font size="4">The Solution</font>
+                            </Header>
+                            <Button icon fluid basic>
+                                <Icon name='angle down' />
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Icon name="users" size='huge' style={{color: "#e91e63"}}></Icon>
+                            <Header >
+                                <font size="4">Our Impact</font>
+                            </Header>
+                            <Button icon fluid basic>
+                                <Icon name='angle down' />
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
       </Segment>
-      <Segment vertical>
-        <p>
-          Blah blah 
-        </p>
-
+      <Segment name="problem">
+            <Header as='h2' textAlign='center'>
+                <Header.Content>The Problem</Header.Content>
+            </Header>
+            <Header as='h3' textAlign='center'>
+                <Header.Content>
+                    These are all the problems and their rationale. 
+                </Header.Content>
+            </Header>
       </Segment>
-
+        <Segment >
+            <Header as='h2' textAlign='center'>
+                <Header.Content>The Solution</Header.Content>
+            </Header>
+            <Header as='h3' textAlign='center'>
+                <Header.Content>
+                    These are all the solutions and their rationale. 
+                </Header.Content>
+            </Header>
+      </Segment>
+        <Segment >
+            <Header as='h2' textAlign='center'>
+                <Header.Content>Our Impact</Header.Content>
+            </Header>
+            <Header as='h3' textAlign='center'>
+                <Header.Content>
+                    Here are all the ways we're making an impact. 
+                </Header.Content>
+            </Header>
+      </Segment>
     </Container>
   );
 };
